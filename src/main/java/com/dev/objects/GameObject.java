518df0fa820a204e7109ee1211f6b7bc1af038a7
Hanny <hannylevy63@gmail.com>
2022-12-15 21:43:31 +0200

    commit1

--- src/main/java/com/dev/objects/GameObject.java
+++ src/main/java/com/dev/objects/GameObject.java
@@ -2,14 +2,28 @@ package com.dev.objects;
 
 import java.util.Date;
 
+
 public class GameObject {
+    private String team1;
+    private String team2;
 
-    private TeamObject team1 ;
-    private TeamObject team2 ;
-    private Date date ;
-    private TeamObject winningTeam;
 
+    public GameObject(String team1, String team2) {
+        // Initialize the game with the names of the teams and a score of 0-0.
+        this.team1 = team1;
+        this.team2 = team2;
+
+    }
+
+    public String getTeam1() {
+        return this.team1;
+    }
+
+    public String getTeam2() {
+        return this.team2;
+    }
 
 
 
 }
+
